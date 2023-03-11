fb37c97ac0ee4fdd6717a4b113d20b14aea2704f
Johan Solbakken <johsol@stud.ntnu.no>
2023-03-11 22:31:29 +0100

    Sphere in the center

--- src/renderer.rs
+++ src/renderer.rs
@@ -24,10 +24,11 @@ impl Renderer {
     pub fn render(&mut self) {
         for y in 0..self.image.get_height() {
             for x in 0..self.image.get_width() {
-                let coord = glm::vec2(
+                let mut coord = glm::vec2(
                     x as f32 / self.image.get_width() as f32,
                     y as f32 / self.image.get_height() as f32,
                 );
+                coord = coord * 2.0 - 1.0;
 
                 let color = self.per_pixel(coord);
 
@@ -39,6 +40,21 @@ impl Renderer {
     fn per_pixel(&mut self, coord: Vector2<f32>) -> Color {
         let r = (coord.x * 255.0) as u8 as u32;
         let g = (coord.y * 255.0) as u8 as u32;
-        0xff000000 | (g << 8) | r
+
+        let ray_direction = glm::vec3(coord.x, coord.y, -1.0);
+        let ray_origin = glm::vec3(0.0, 0.0, 2.0);
+        let radius = 0.5;
+
+        let a = glm::dot(ray_direction, ray_direction);
+        let b = 2.0 * glm::dot(ray_direction, ray_origin);
+        let c = glm::dot(ray_origin, ray_origin) - radius * radius;
+
+        let discriminant = b * b - 4.0 * a * c;
+
+        if discriminant >= 0.0 {
+            return 0xffff00ff;
+        }
+
+        0xff000000
     }
 }
