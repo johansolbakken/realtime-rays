254967e1cd2720450d367869f32432bb1b6cc10f
Johan Solbakken <johsol@stud.ntnu.no>
2023-03-11 22:20:41 +0100

    Draw gradient

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,6 @@
-use crate::image::Image;
+use glm::Vector2;
+
+use crate::image::{Color, Image};
 
 pub struct Renderer {
     image: Image,
@@ -22,8 +24,21 @@ impl Renderer {
     pub fn render(&mut self) {
         for y in 0..self.image.get_height() {
             for x in 0..self.image.get_width() {
-                self.image.set_pixel(x, y, 0xff00ff00); // ABGR
+                let coord = glm::vec2(
+                    x as f32 / self.image.get_width() as f32,
+                    y as f32 / self.image.get_height() as f32,
+                );
+
+                let color = self.per_pixel(coord);
+
+                self.image.set_pixel(x, y, color); // ABGR
             }
         }
+    }
+
+    fn per_pixel(&mut self, coord: Vector2<f32>) -> Color {
+        let r = (coord.x * 255.0) as u8 as u32;
+        let g = (coord.y * 255.0) as u8 as u32;
+        0xff000000 | (g << 8) | r
     }
 }
